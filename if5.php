85e110f0bcacf547effe2ba9ec22afc7fec2d664
RaihanFirmani <43979425+RaihanFirmani@users.noreply.github.com>
2018-10-26 08:37:52 +0700

    Add files via upload

---
+++ if5.php
@@ -0,0 +1,43 @@
+<?php
+$text=$_POST['jk1'];
+$text1=$_POST['jk2'];
+$text2=$_POST['jk3'];
+$text3=$_POST['jk4'];
+$text4=$_POST['a'];
+$text5=$_POST['b'];
+$text6=$_POST['c'];
+$text7=$_POST['d'];
+
+if($text='pria')
+	{
+		echo "<font color=blue>$text4</font><br>";
+	}
+else
+	{
+		echo "<font color=red>$text4</font><br>";
+	}
+if($text1='pria')
+	{
+		echo "<font color=blue>$text5</font><br>";
+	}
+else
+	{
+		echo "<font color=red>$text5</font><br>";
+	}
+if($text2='pria')
+	{
+		echo "<font color=blue>$text6</font><br>";
+	}
+else
+	{
+		echo "<font color=red>$text6</font><br>";
+	}
+if($text3='pria')
+	{
+		echo "<font color=blue>$text7</font><br>";
+	}
+else
+	{
+		echo "<font color=red>$text7</font><br>";
+	}
+	?>
